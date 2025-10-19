335249b8243cc519fa0bdfaa52bca497ddeb5903
Hun Namkung <hnamkung15@gmail.com>
2025-10-18 23:40:19 -0700

    minor fix

--- services/plot/chart_service.py
+++ services/plot/chart_service.py
@@ -84,12 +84,15 @@ def total_portfolio_pie_chart(
 
     # 현금도 카테고리에 포함
     usd_cash_in_usd = usd_portfolio.cash
+    ticker_map["USD Cash"] = "USD 현금"
+    individuals["Stable Assets"]["USD Cash"] = usd_cash_in_usd
+
+    category_totals["Stable Assets"] += usd_cash_in_usd
+
     # krw_cash_in_usd = krw_portfolio.cash / fx_rate
     # category_totals["Stable Assets"] += usd_cash_in_usd + krw_cash_in_usd
-    category_totals["Stable Assets"] += usd_cash_in_usd
-    ticker_map["USD Cash"] = "USD 현금"
+
     # ticker_map["KRW Cash"] = "KRW 현금"
-    individuals["Stable Assets"]["USD Cash"] = usd_cash_in_usd
     # individuals["Stable Assets"]["KRW Cash"] = krw_cash_in_usd
 
     category_labels = []
